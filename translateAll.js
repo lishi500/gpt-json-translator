c67fcd92d6b7e2d2942cac1fc0b457fe37b56485
shli <shli@tripadvisor.com>
2024-01-30 17:46:22 +1100

    fix func name

--- translateAll.js
+++ translateAll.js
@@ -3,11 +3,11 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import config from "./src/config.js";
-import { translateOne } from "./translateLocale.js";
+import { _translateOne } from "./translateLocale.js";
 
 export async function _translateAll() {
   for (const locale of config.supportedLocales) {
-    if (locale !== config.sourceLocale) translateOne(locale);
+    if (locale !== config.sourceLocale) _translateOne(locale);
   }
 }
 
